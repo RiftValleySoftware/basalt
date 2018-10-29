dbbfc7b41f1ac15b6ae10bd42267e0a0fe12aa1f
Chris M <webmaster@magnaws.com>
2018-10-29 05:26:44 -0400

    Forgot to update the README. Updated the submodule.

--- callREST.php
+++ callREST.php
@@ -3,13 +3,22 @@
 /**
     BASALT Extension Layer
 
-    © Copyright 2018, Little Green Viper Software Development LLC.
-
-    This code is proprietary and confidential code, 
-    It is NOT to be reused or combined into any application,
-    unless done so, specifically under written license from Little Green Viper Software Development LLC.
+    © Copyright 2018, Little Green Viper Software Development LLC/The Great Rift Valley Software Company
+    
+    LICENSE:
+    
+    FOR OPEN-SOURCE (COMMERCIAL OR FREE):
+    This code is released as open source under the GNU Plublic License (GPL), Version 3.
+    You may use, modify or republish this code, as long as you do so under the terms of the GPL, which requires that you also
+    publish all modificanions, derivative products and license notices, along with this code.
+    
+    UNDER SPECIAL LICENSE, DIRECTLY FROM LITTLE GREEN VIPER OR THE GREAT RIFT VALLEY SOFTWARE COMPANY:
+    It is NOT to be reused or combined into any application, nor is it to be redistributed, republished or sublicensed,
+    unless done so, specifically WITH SPECIFIC, WRITTEN PERMISSION from Little Green Viper Software Development LLC,
+    or The Great Rift Valley Software Company.
 
     Little Green Viper Software Development: https://littlegreenviper.com
+    The Great Rift Valley Software Company: https://riftvalleysoftware.com
 */
 /// If __DISPLAY_BASICS__ is set to true, then the call_REST_API function will display the basic URI and API key as echos.
 define ('__DISPLAY_BASICS__', true);
